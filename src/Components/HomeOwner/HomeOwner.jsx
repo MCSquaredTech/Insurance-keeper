824a3c422b780b0b7b841009de6f1cdd23b7756b
Mark Christensen <mark@mcsquared.tech>
2023-08-23 11:50:53 -0600

    Getting Started

--- src/Components/HomeOwner/HomeOwner.jsx
+++ src/Components/HomeOwner/HomeOwner.jsx
@@ -4,14 +4,33 @@ import './HomeOwner.css'
 const HomeOwner = () => {
   return (
     <div className="formBox">
-      <form action="" className="form-group row">
-        <label htmlFor="firstName" className="col-sm-4 col-form-label">First Name:</label>
-        <input type="text" 
-              className="form-control-plaintext" 
-              id="firstName" 
-              value="" 
-              size={12}
-              placeholder='First Name' />
+      <form action="" className="form">
+        <div className="form-group row">
+          <label htmlFor="firstName" className="col-sm-4 col-form-label">First Name:</label>
+          <input type="text" 
+                className="form-control-plaintext" 
+                id="firstName" 
+                size={"30"}
+                placeholder='First Name' />
+        </div>
+        <div className="form-group row">
+          <label htmlFor="lastName" className="col-sm-4 col-form-label">Last Name:</label>
+          <input type="text" 
+                className="form-control-plaintext" 
+                id="lastName" 
+                size={"30"}
+                placeholder='Last Name' />
+        </div>
+        <div className="form-group row">
+          <label htmlFor="email" className="col-sm-4 col-form-label">Email Address:</label>
+          <input type="email" 
+                className="form-control-plaintext" 
+                id="email" 
+                size={"24"}
+                readOnly={false}
+                placeholder='Email Address' />
+        </div>
+        
       </form>
     </div>
   )
