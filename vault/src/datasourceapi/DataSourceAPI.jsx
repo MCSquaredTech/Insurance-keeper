a487a6cc26ba870ec586d2b2bb753e22b6e89d2e
Mark D Christensen <mark@brooksnet.com>
2023-08-31 16:24:02 -0600

    Fetch

--- vault/src/datasourceapi/DataSourceAPI.jsx
+++ vault/src/datasourceapi/DataSourceAPI.jsx
@@ -1,7 +1,44 @@
 
-const MockIoApi = process.env.REACT_APP_MOCKIO_API;
+const mockIoAPI = process.env.REACT_APP_MOCKIO_API;
 
 class DataSourceApi { 
+
+    getUsers = async () => { 
+        fetch(`${mockIoAPI}/user`)
+    }
+
+    getUserById = async (User) => { 
+
+    }
+
+    postUser = async (User) => { 
+
+    }
+
+    putUser = async (User) => { 
+
+    }
+
+    deleteUser = async (User) => { 
+
+    }
+
+    getAddressByUser = async (User) => { 
+
+    }
+
+    postAddress = async (User, Address) => { 
+
+    }
+
+    putAddress = async (User, Address) => { 
+
+    }
+
+    deleteAddress = async (User, Address) => { 
+
+    }
+
 
 
 }
